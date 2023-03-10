d1c25907e61a90526ba788222b813bc54719901d
George <gio.digmel@gmail.com>
2023-03-10 16:24:45 -0600

    fix: updated keys

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,23 +9,29 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+  try {
+    const configuration = new Configuration({
+      apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: `return json file with "incorrect", "hint", and "corrected" keys. "incorrect" should be one interesting sentence but grammatically wrong with multiple mistakes, chose advanced grammatical mistakes such as conditional statements, complex tenses, misused phrasal verbs, missed punctuation and etc, "hint" should be a little hint about what was wrong with "incorrect", and "corrected" should be exact sentence with correct grammar.`,
-      },
-    ],
-  });
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `return json file with "incorrect", "hint", and "corrected" keys. "incorrect" should be one interesting sentence but grammatically wrong with multiple mistakes, chose advanced grammatical mistakes such as conditional statements, complex tenses, misused phrasal verbs, missed punctuation and etc, "hint" should be a little hint about what was wrong with "incorrect", and "corrected" should be exact sentence with correct grammar.`,
+        },
+      ],
+    });
 
-  console.log("first", completion.data.choices[0].message?.content);
+    console.log("first", completion.data.choices[0].message?.content);
 
-  const result2 = completion ? completion.data.choices[0].message?.content : "";
-  res.status(200).json({ result: result2 });
+    const result2 = completion
+      ? completion.data.choices[0].message?.content
+      : "";
+    res.status(200).json({ result: result2 });
+  } catch ({ message }) {
+    console.log("Error connected with openAI API", message);
+  }
 }
